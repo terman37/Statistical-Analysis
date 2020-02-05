25ea6e16d03e7df7f30150339b66611d5441fca3
terman37 <terman37@yahoo.fr>
2020-02-05 22:31:55 +0100

    Update functionBestCART.R

--- cart-randomforest/functionBestCART.R
+++ cart-randomforest/functionBestCART.R
@@ -1,17 +1,30 @@
-# package about CART
-library(rpart)
 
 BestTree <- function(X,Y){
-
+  # package about CART
+  library(rpart)
+  # generate max tree
   TreeMAX = rpart(Y~.,data=X,control = rpart.control(cp=10^-9,minsplit=2))
-  # plotcp(TreeMAX)
+  # find cp corresponding to OneSE
   CPs=TreeMAX$cptable
   smallest_xerror = which(CPs[,'xerror'] == min(CPs[,'xerror']))
   oneSE = CPs[smallest_xerror,'xerror']+CPs[smallest_xerror,'xstd']
   CPBestTree = head(CPs[CPs[,'xerror']<=oneSE,],1)[,'CP']
-
-  # BestTree = rpart(Y~.,data=X,control = rpart.control(cp=CPBestTree,minsplit=2))
+  # Prune Tree and return best tree
   BestTree = prune(TreeMAX,cp=CPBestTree)
+}
+
+Bagging <- function(X,Y,nbtree=5,nbobs=30){
+  bagtree = list()
+  # generate nb trees
+  for (k in 1:nbtree) {
+    rows_selected = sample(1:nrow(X),nbobs,replace = TRUE)
+    newX = X[rows_selected,]
+    newY = Y[rows_selected]
+    Treek = BestTree(newX,newY)
+    bagtree = append(bagtree,list(Treek))
+    names(bagtree)[k] <- paste("Tree",k)
+  }
+  Bagging <- bagtree
 }
 
 data(mtcars)
@@ -19,8 +32,19 @@ data(mtcars)
 X = mtcars[,-1]
 Y = mtcars[,1]
 
-Tree = BestTree(X,Y)
-plot(Tree)
-text(Tree)
-printcp(Tree)
+Trees = Bagging(X,Y,nbtree=10,nbobs=30)
+
+newobs=as.list(c(disp=183,hp=175,drat=3.45,wt=2.42,qsec=18.5,vs=1,am=0,gear=3,carb=2))
+predict(Trees[[1]], newdata=newobs)
+result = 0
+for (k in 1:length(Trees)) {
+  result = result + 1/k *predict(Trees[[k]], newdata=newobs)
+}
+
+
+
+# Tree = BestTree(X,Y)
+# plot(Tree)
+# text(Tree)
+# printcp(Tree)
 
